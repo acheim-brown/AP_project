b046b0ce4eec5c922b66d761529a62fda1e225b9
crystalfletcher <44850787+crystalfletcher@users.noreply.github.com>
2021-11-28 21:44:58 -0500

    Add files via upload

--- MainPage.java
+++ MainPage.java
@@ -24,8 +24,6 @@ public class MainPage extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		
-		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
